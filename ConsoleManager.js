eb743239448e3578742dbe469d111f1bef1d4198
xPyth <xpyth@protonmail.com>
2022-03-21 01:12:27 +0100

    Edited  [constructor].Show, was also Hiding the console

--- ConsoleManager.js
+++ ConsoleManager.js
@@ -1,4 +1,4 @@
 var ConsoleWindow = require("node-hide-console-window");
 
 exports.Hide = function () { ConsoleWindow.hideConsole(); }
-exports.Show = function () { ConsoleWindow.hideConsole(); }
+exports.Show = function () { ConsoleWindow.showConsole(); }
